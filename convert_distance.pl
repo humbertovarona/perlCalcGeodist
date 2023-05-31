a6a58a2d50dbf1710fd711ffc415af7943da6516
Humberto L. Varona <humberto.varona@yandex.com>
2023-05-30 21:25:25 -0300

    Update convert_distance.pl

--- convert_distance.pl
+++ convert_distance.pl
@@ -5,7 +5,7 @@ sub convert_distance {
         return $distance * 111.12;
     } elsif ($unit eq "miles") {
         return $distance * 69.046766881413;  
-    } elsif ($unit eq "nautical miles") {
+    } elsif ($unit eq "nm") {
         return $distance * 60;  
     } else {
         die "Invalid unit. Supported units are 'km', 'miles', and 'nm'.";
